27a8f2636e94fa3df4a9f879f2321becceddd05d
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-12 14:10:30 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,4 +11,5 @@ fireclickdetector(workspace.Orb.ClickDetector)
 ]])
 end
 game:GetService("StarterGui"):SetCore("SendNotification",{Title = "be patient",Text = "plz wait script is loading rn" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
+wait(2)
 game:GetService("TeleportService"):Teleport(74169485398268)
