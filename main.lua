2a384b5f05f48fc03bf904dec79db935f544a698
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2024-10-12 14:05:48 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,9 +5,10 @@ local teleportFunc = queueonteleport or queue_on_teleport
                 game.Loaded:Wait()
             end
             repeat wait() until game.Players.LocalPlayer
-game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Workspace.Orb.CFrame
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = workspace.Orb.CFrame
 wait(0.5)
 fireclickdetector(workspace.Orb.ClickDetector)
 ]])
 end
+game:GetService("StarterGui"):SetCore("SendNotification",{Title = "be patient",Text = "plz wait script is loading rn" ,Duration = 10, Icon = "rbxthumb://type=Asset&id=9649923610&w=150&h=150",Button1 = "alright dud"})
 game:GetService("TeleportService"):Teleport(74169485398268)
